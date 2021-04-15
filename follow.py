bb771b8b88c7b611375ee76204a6f83cdd39771c
valentino1337 <80648308+valentino1337@users.noreply.github.com>
2021-04-15 20:53:11 +0200

    follow
    made follow ~kinda~ working
    if you stumble upon people you already follow, it will crash, so make sure you unfollow everyone before choosing a target
    its a jank city fix, but instagram is retarded and this is the only way i found

--- follow.py
+++ follow.py
@@ -1,18 +1,66 @@
-from instapy import InstaPy
+import time
 import random
-cooldown = random.uniform(2, 3.5)
+from selenium import webdriver
+gurun = str(input('Username to follow followers?: '))
+profile = webdriver.FirefoxProfile()
+profile.set_preference("general.useragent.override", "Mozilla/5.0 (iPhone; CPU iPhone OS 12_1 like Mac OS X) AppleWebKit/605.1.15 " "(KHTML, like Gecko) FxiOS/18.1 Mobile/16B92 Safari/605.1.15")
+driver = webdriver.Firefox(profile)
+
+cooldown = random.uniform(2.5, 3.5)
+unfollowcooldown = random.uniform(900, 930)
 
 from creds import username
 from creds import password
-session = InstaPy(username= str(username), password= str(password))
-session.login()
-session.set_skip_users(skip_private=False)
 
-print("===========================")
-print("DO NOT TERMINATE THE TASK")
-print("===========================")
+#driver.set_window_size(853, 853)
+driver.set_window_size(480, 853)
+driver.get('https://www.instagram.com/accounts/login/')
+time.sleep(3)
+driver.find_element_by_xpath("/html/body/div[2]/div/div/div/div[2]/button[1]").click()
+time.sleep(1)
+driver.find_element_by_xpath('//*[@id="loginForm"]/div[1]/div[3]/div/label/input').send_keys(username)
+driver.find_element_by_xpath('//*[@id="loginForm"]/div[1]/div[4]/div/label/input').send_keys(password)
+driver.find_element_by_xpath('//*[@id="loginForm"]/div[1]/div[6]/button').click()
+time.sleep(5)
+driver.find_element_by_xpath('/html/body/div[1]/section/main/div/div/section/div/button').click()
+time.sleep(1)
+print('Successfully logged in')
+driver.get('https://www.instagram.com/' + gurun)
+time.sleep(1.5)
+driver.find_element_by_xpath('//*[@id="react-root"]/section/main/div/ul/li[2]/a/span').click()
+time.sleep(3)
+print('Starting to follow')
 
-from followerlist import tofollow
-session.follow_by_list(tofollow, times=1, sleep_delay=float(cooldown), interact=False)
+#while driver.find_elements_by_class_name('sqdOP.L3NKy._8A5w5'):
+#    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
-session.end()
+follownu = 3
+unfollowed = 0
+while unfollowed <= 2:
+    #while driver.find_element_by_class_name('sqdOP.L3NKy._8A5w5'):
+    while driver.find_elements_by_xpath('/html/body/div[1]/section/main/div/ul/div/li[' + str(follownu) + ']/div/div[2]/button'):
+        scrollcounter = 0
+        while scrollcounter <= 11:
+            driver.find_element_by_xpath('/html/body/div[1]/section/main/div/ul/div/li[' + str(follownu) + ']/div/div[2]/button').click()
+            time.sleep(cooldown)
+            follownu = follownu + 1
+            scrollcounter = scrollcounter + 1
+            print('Followed the the ' + str(scrollcounter) + '. user')
+        while scrollcounter == 11:
+            time.sleep(0.5)
+            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            time.sleep(0.5)
+            scrollcounter = scrollcounter - 12
+            unfollowed = unfollowed + 1
+            print('Scrolled for the ' + str(unfollowed) + '. time')
+while unfollowed == 3:
+    timeout = 0
+    while timeout == 0:
+        print("Chilling for a little bit so Instagram doesn't whoop your ass")
+        time.sleep(float(unfollowcooldown))
+        timeout = timeout + 1
+    while timeout == 1:
+        unfollowed = unfollowed - 3
+
+
+print('Followed everyone')
---
+++ follow_from_a_list.py
@@ -0,0 +1,18 @@
+from instapy import InstaPy
+import random
+cooldown = random.uniform(2, 3.5)
+
+from creds import username
+from creds import password
+session = InstaPy(username= str(username), password= str(password))
+session.login()
+session.set_skip_users(skip_private=False)
+
+print("===========================")
+print("DO NOT TERMINATE THE TASK")
+print("===========================")
+
+from scraped import listt
+session.follow_by_list(listt, times=1, sleep_delay=float(cooldown), interact=False)
+
+session.end()
